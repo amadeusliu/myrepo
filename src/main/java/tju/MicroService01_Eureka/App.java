2666b9732035b2a0380230748f6a202ba7e1d6fe
amadeusliu <amadeusliu@sina.com>
2020-11-28 16:10:28 +0800

    new one

--- src/main/java/tju/MicroService01_Eureka/App.java
+++ src/main/java/tju/MicroService01_Eureka/App.java
@@ -2,10 +2,8 @@ package tju.MicroService01_Eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
 public class App 
 {
     public static void main( String[] args )
