693c104d425adfb1b633a195021138f682e93532
amadeusliu <amadeusliu@sina.com>
2020-11-28 20:50:52 +0800

    t

--- src/main/java/tju/MicroService01_Eureka/App.java
+++ src/main/java/tju/MicroService01_Eureka/App.java
@@ -2,12 +2,19 @@ package tju.MicroService01_Eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class App 
+public class App extends SpringBootServletInitializer
 {
     public static void main( String[] args )
     {
         SpringApplication.run(App.class,args);
+    }
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    	return builder.sources(App.class);
     }
 }
